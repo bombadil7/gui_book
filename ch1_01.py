921ed0951765f774cad59211ee3aafc142e75b9e
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-07-13 08:00:27 -0700

    Set focus to Entry.

--- ch1_01.py
+++ ch1_01.py
@@ -23,6 +23,7 @@ nameEntered.grid(column=0, row=1)
 
 # Adding a Button
 action = ttk.Button(win, text="Click Me!", command=clickMe)
+action.bind("<Return>", (lambda event: clickMe())
 # Position Buttion in second row
 action.grid(column=1, row=1)
 nameEntered.focus()
