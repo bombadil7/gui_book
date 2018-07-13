c7fd06831c02f77fa551d30136ceb9de83c9925a
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-07-13 12:06:25 -0700

    Add Radiobuttons.

--- ch1_01.py
+++ ch1_01.py
@@ -48,6 +48,27 @@ check3 = tk.Checkbutton(win, text="Enabled", variable=chVarEn)
 check3.select() # Default state is checked
 check3.grid(column=2, row=4, sticky=tk.W)
 
+# Radiobutton Globals
+COLOR1 = "Blue"
+COLOR2 = "Gold"
+COLOR3 = "Red"
+
+# Radiobutton Callback
+def radCall():
+    radSel = radVar.get()
+    if   radSel == 1: win.configure(background=COLOR1)
+    elif radSel == 2: win.configure(background=COLOR2)
+    elif radSel == 3: win.configure(background=COLOR3)
+
+# Create three Radiobuttons
+radVar = tk.IntVar()
+rad1 = tk.Radiobutton(win, text=COLOR1, variable=radVar, value=1, command=radCall)
+rad1.grid(column=0, row=5, sticky=tk.W)
+rad2 = tk.Radiobutton(win, text=COLOR2, variable=radVar, value=2, command=radCall)
+rad2.grid(column=1, row=5, sticky=tk.W)
+rad3 = tk.Radiobutton(win, text=COLOR3, variable=radVar, value=3, command=radCall)
+rad3.grid(column=2, row=5, sticky=tk.W)
+
 
 # Adding a Button
 action = ttk.Button(win, text="Click Me!", command=clickMe)
