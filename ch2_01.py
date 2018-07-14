2fa743eb67516daf9eafb030aeda13d5d80c0bf7
Andrei Kniazev <akniazev@gmail.com>
2018-07-14 23:14:52 +0000

    Small adjustments to layout.

--- ch2_01.py
+++ ch2_01.py
@@ -4,10 +4,12 @@ from tkinter import scrolledtext
 
 win = tk.Tk()
 win.title("Python GUI")
-
 #win.resizable(0, 0)     # Disable resizing
+monty = ttk.LabelFrame(win, text=' Monty Python ')
+monty.grid(column=0, row=0)
+
 # Adding a Label
-aLabel = ttk.Label(win, text="A Label")
+aLabel = ttk.Label(monty, text="A Label")
 aLabel.grid(column=0, row=0)
 
 
@@ -15,37 +17,37 @@ def clickMe():
     action.configure(text="Hello " + name.get())
 
 # Changing our Label
-ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
+ttk.Label(monty, text="Enter a name:").grid(column=0, row=0, sticky=tk.W)
 
 # Adding a Textbox Entry widget
 name = tk.StringVar()
-nameEntered = ttk.Entry(win, width=12, textvariable=name)
+nameEntered = ttk.Entry(monty, width=12, textvariable=name)
 nameEntered.bind("<Return>", (lambda event: clickMe()))
-nameEntered.grid(column=0, row=1)
+nameEntered.grid(column=0, row=1, sticky=tk.W)
 
 # Add Combobox
-ttk.Label(win, text="Choose a number:").grid(column=1, row=0)
+ttk.Label(monty, text="Choose a number:").grid(column=1, row=0)
 number = tk.StringVar()
-#numberChosen = ttk.Combobox(win, width=12, textvariable=number)
+#numberChosen = ttk.Combobox(monty, width=12, textvariable=number)
 # To prevent user from typing their own number in the field add 'state' prop:
-numberChosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+numberChosen = ttk.Combobox(monty, width=12, textvariable=number, state='readonly')
 numberChosen['values'] = (1, 2, 4, 42, 100)
 numberChosen.grid(column=1, row=1)
 numberChosen.current(0)
 
 # Creating three checkbuttons
 chVarDis = tk.IntVar()
-check1 = tk.Checkbutton(win, text="Disabled", variable=chVarDis, state='disabled')
+check1 = tk.Checkbutton(monty, text="Disabled", variable=chVarDis, state='disabled')
 check1.select()  # Default state is checked
 check1.grid(column=0, row=4, sticky=tk.W)
 
 chVarUn = tk.IntVar()
-check2 = tk.Checkbutton(win, text="UnChecked", variable=chVarUn)
+check2 = tk.Checkbutton(monty, text="UnChecked", variable=chVarUn)
 check2.deselect() # Default state is unchecked
 check2.grid(column=1, row=4, sticky=tk.W)
 
 chVarEn = tk.IntVar()
-check3 = tk.Checkbutton(win, text="Enabled", variable=chVarEn)
+check3 = tk.Checkbutton(monty, text="Enabled", variable=chVarEn)
 check3.select() # Default state is checked
 check3.grid(column=2, row=4, sticky=tk.W)
 
@@ -56,9 +58,9 @@ colors = ["Blue", "Gold", "Red"]
 # Radiobutton Callback
 def radCall():
     radSel = radVar.get()
-    if   radSel == 0: win.configure(background=colors[0])
-    elif radSel == 1: win.configure(background=colors[1])
-    elif radSel == 2: win.configure(background=colors[2])
+    if   radSel == 0: monty.configure(background=colors[0])
+    elif radSel == 1: monty.configure(background=colors[1])
+    elif radSel == 2: monty.configure(background=colors[2])
 
 # Create three Radiobuttons using one variable
 radVar = tk.IntVar()
@@ -68,7 +70,7 @@ radVar.set(99)
 # Now we create all three Radiobutton widgets within one loop
 for col in range(3):
     curRad = 'rad' + str(col)
-    curRad = tk.Radiobutton(win, text=colors[col], variable=radVar, 
+    curRad = tk.Radiobutton(monty, text=colors[col], variable=radVar, 
             value=col, command=radCall)
     curRad.grid(column=col, row=6, sticky=tk.W)
 
@@ -76,26 +78,26 @@ for col in range(3):
 scrolW = 30
 scrolH = 3
 
-scr = scrolledtext.ScrolledText(win, width=scrolW, height=scrolH, wrap=tk.WORD)
+scr = scrolledtext.ScrolledText(monty, width=scrolW, height=scrolH, wrap=tk.WORD)
 scr.grid(column=0, columnspan=3, row=5)
 #scr.grid(column=0, columnspan=3, row=5, sticky='WE')
 # Adding a Button
-action = ttk.Button(win, text="Click Me!", command=clickMe)
+action = ttk.Button(monty, text="Click Me!", command=clickMe)
 action.configure(state='disabled')  # Disable the Button Widget
 # Position Buttion in second row
 action.grid(column=2, row=1)
 
 # Create a container to hold labels
-labelsFrame = ttk.LabelFrame(win, text=' Labels in a Frame ')
-labelsFrame.grid(column=1, row=7, padx=20, pady=30)
+labelsFrame = ttk.LabelFrame(monty, text=' Labels in a Frame ')
+labelsFrame.grid(column=0, row=7, padx=20, pady=30)
 
 # Place labels into the container element
-ttk.Label(labelsFrame, text="Label1").grid(column=0, row=0)
-ttk.Label(labelsFrame, text="Label2").grid(column=1, row=0)
-ttk.Label(labelsFrame, text="Label3").grid(column=2, row=0)
+ttk.Label(labelsFrame, text="Label1").grid(column=0, row=0, sticky='W')
+ttk.Label(labelsFrame, text="Label2").grid(column=0, row=1, sticky='W')
+ttk.Label(labelsFrame, text="Label3").grid(column=0, row=2, sticky='W')
 
 for child in labelsFrame.winfo_children():
     child.grid_configure(padx=8, pady=4)
 nameEntered.focus()
 
-win.mainloop()
+monty.mainloop()
