6a00f21db4ad30a3674ace158d8c1dd0db82bc8d
Andrei Kniazev <akniazev@gmail.com>
2018-07-18 01:39:39 +0000

    Set min and max for x and y dynamically.

--- ch5_01.py
+++ ch5_01.py
@@ -6,33 +6,43 @@ import tkinter as tk
 fig = Figure(figsize=(12, 8), facecolor='white')
 #--------------------------------------------------------------
 # axis = fig.add_subplot(111)   # 1 row, 1 column, only graph
-axis = fig.add_subplot(211)   # 2 rows, 1 column, Top graph
+axis = fig.add_subplot(111)   # 2 rows, 1 column, Top graph
 #--------------------------------------------------------------
 xValues = [1, 2, 3, 4]
 
 yValues0 = [6, 7.5, 8, 7.5]
 yValues1 = [5.5, 6.5, 50, 6]
 yValues2 = [6.5, 7, 8, 7]
+yAll = [yValues0, yValues0, yValues2]   # list of lists
+
+# flatten list of lists retrieving minimum value
+minY = min([y for yValues in yAll for y in yValues])
+
+yUpperLimit = 20
+# flatten list of lists retrieving max value within defined limit
+maxY = max([y for yValues in yAll for y in yValues if y < yUpperLimit])
 
 t0, = axis.plot(xValues, yValues0)
 t1, = axis.plot(xValues, yValues1)
 t2, = axis.plot(xValues, yValues2)
 
+axis.set_ylim(minY, maxY)
+axis.set_xlim(min(xValues), max(xValues))
+
 axis.set_xlabel('Horizontal Label')
 axis.set_ylabel('Vertical Label')
-axis.set_ylim(5, 8)
 
 # axis.grid()   # default line style
 axis.grid(linestyle='-')  
 
 fig.legend((t0, t1, t2), ('First line', 'Second line', 'Third line'), 'upper right')
 #--------------------------------------------------------------
-axis1 = fig.add_subplot(212)   # 2 rows, 1 column, Top graph
-#--------------------------------------------------------------
-xValues1 = [1, 2, 3, 4]
-yValues1 = [7, 5, 8, 6]
-axis1.plot(xValues1, yValues1)
-axis1.grid()   # default line style
+#axis1 = fig.add_subplot(212)   # 2 rows, 1 column, Top graph
+##--------------------------------------------------------------
+#xValues1 = [1, 2, 3, 4]
+#yValues1 = [7, 5, 8, 6]
+#axis1.plot(xValues1, yValues1)
+#axis1.grid()   # default line style
 #--------------------------------------------------------------
 def _destroyWindow():
     root.quit()
